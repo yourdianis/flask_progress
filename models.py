496857d1ef033059b8def3a12e0e658f930a7659
yourdianis <3275594770@qq.com>
2024-07-19 13:52:05 +0800

    “修正username字段错误”

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class UserModel(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    usrname = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     email = db.Column(db.String(64), nullable=False, unique=True)
     # phone = db.Column(db.String(64), nullable=False, unique=True)
     join_time = db.Column(db.DateTime, default=datetime.now)
@@ -16,4 +16,4 @@ class EmailCaptchaModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(64), nullable=False, unique=True)
     # phone = db.Column(db.String(64), nullable=False, unique=True)
-    captcha = db.Column(db.DateTime, default=datetime.now)
+    captcha = db.Column(db.String(64), default=datetime.now)
